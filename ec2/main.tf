8a71d0cd7d5b054edcb9ecdf49f32b069dff7841
pavani1105 <129930898+pavani1105@users.noreply.github.com>
2023-09-20 11:51:34 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,12 +1,11 @@
-resource "aws_instance" "ec2_instance" {
-  ami = "ami-08a52ddb321b32a8c"
-  instance_type = "t2.micro"
+resource "aws_instance" "webapp" {
+  ami = var.ami_id
+  instance_type = var.instance_type
   subnet_id = var.public_subnet_id
-  #subnet_id = aws_subnet.publicsubnet.id
   key_name = "test"
   associate_public_ip_address = "true"
 
   tags = {
-    Name = "ec2_instance"
+    Name = "webapp"
   }
 }
